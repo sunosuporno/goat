779091a2250df5fed2639023e9336c8f6ec7959c
sunosuporno <sarkarsuporno36@gmail.com>
2024-12-19 04:24:48 +0530

    Add parameters, add ezETH token in types

---
+++ typescript/packages/plugins/renzo/src/parameters.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const depositSchema = z.object({
+    _amountIn: z.string().describe("The amount of tokens to deposit"),
+    _minOut: z.string().describe("The minimum amount of tokens to receive"),
+    _deadline: z.string().describe("The deadline for the deposit"),
+});
+
+export const depositETHSchema = z.object({
+    _minOut: z.string().describe("The minimum amount of ETH to receive"),
+    _deadline: z.string().describe("The deadline for the deposit"),
+});
+
+export const balanceOfSchema = z.object({
+    _address: z.string().describe("The address to check the balance of"),
+});
--- typescript/packages/plugins/renzo/src/types/ChainSpecifications.ts
+++ typescript/packages/plugins/renzo/src/types/ChainSpecifications.ts
@@ -4,35 +4,41 @@ export type ChainSpecifications = Record<
     number,
     {
         renzoDepositAddress: Address;
+        l2EzEthAddress: Address;
     }
 >;
 
 export const RenzoOnMode: ChainSpecifications = {
     [mode.id]: {
         renzoDepositAddress: "0x4D7572040B84b41a6AA2efE4A93eFFF182388F88",
+        l2EzEthAddress: "0x2416092f143378750bb29b79eD961ab195CcEea5",
     },
 };
 
 export const RenzoOnBase: ChainSpecifications = {
     [base.id]: {
         renzoDepositAddress: "0xf25484650484de3d554fb0b7125e7696efa4ab99",
+        l2EzEthAddress: "0x2416092f143378750bb29b79eD961ab195CcEea5",
     },
 };
 
 export const RenzoOnArbitrum: ChainSpecifications = {
     [arbitrum.id]: {
         renzoDepositAddress: "0xf25484650484de3d554fb0b7125e7696efa4ab99",
+        l2EzEthAddress: "0x2416092f143378750bb29b79eD961ab195CcEea5",
     },
 };
 
 export const RenzoOnBsc: ChainSpecifications = {
     [bsc.id]: {
         renzoDepositAddress: "0xf25484650484de3d554fb0b7125e7696efa4ab99",
+        l2EzEthAddress: "0x2416092f143378750bb29b79eD961ab195CcEea5",
     },
 };
 
 export const RenzoOnLinea: ChainSpecifications = {
     [linea.id]: {
         renzoDepositAddress: "0x4D7572040B84b41a6AA2efE4A93eFFF182388F88",
+        l2EzEthAddress: "0x2416092f143378750bb29b79eD961ab195CcEea5",
     },
 };
