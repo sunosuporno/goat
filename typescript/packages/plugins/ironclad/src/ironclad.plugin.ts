49b37a89360dcc4322eec0d5ace60530c337acca
sunosuporno <sarkarsuporno36@gmail.com>
2024-12-19 15:36:40 +0530

    Remove CtorParams, add index.ts

--- typescript/packages/plugins/ironclad/src/ironclad.plugin.ts
+++ typescript/packages/plugins/ironclad/src/ironclad.plugin.ts
@@ -1,7 +1,4 @@
-import type {
-    IronCladCtorParams,
-    IronCladContractAddresses,
-} from "./types/IroncladCtorParams";
+import { IroncladService } from "./ironclad.service";
 import { type Chain, PluginBase, ToolBase } from "@goat-sdk/core";
 import { mode } from "viem/chains";
 import type { EVMWalletClient } from "@goat-sdk/wallet-evm";
@@ -9,15 +6,12 @@ import type { EVMWalletClient } from "@goat-sdk/wallet-evm";
 const SUPPORTED_CHAINS = [mode];
 
 export class IronCladPlugin extends PluginBase<EVMWalletClient> {
-    private readonly addresses: IronCladContractAddresses;
-    constructor(params: IronCladCtorParams) {
-        super("ironclad", []);
-        this.addresses = params.addresses;
+    constructor() {
+        super("ironclad", [new IroncladService()]);
     }
 
     supportsChain = (chain: Chain) =>
         chain.type === "evm" && SUPPORTED_CHAINS.some((c) => c.id === chain.id);
 }
 
-export const ironclad = (params: IronCladCtorParams) =>
-    new IronCladPlugin(params);
+export const ironclad = () => new IronCladPlugin();
